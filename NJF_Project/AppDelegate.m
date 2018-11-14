49e2947b761be344fbe9e73f9b010e0fe7011b04
niujf <niujf@kingnet.com>
2018-11-14 20:38:06 +0800

    广告

--- NJF_Project/AppDelegate.m
+++ NJF_Project/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "NJF_TabBarController.h"
+#import "XHLaunchAd.h"
 
 @interface AppDelegate ()
 
@@ -23,6 +24,7 @@
     self.window.frame = [UIScreen mainScreen].bounds;
     NJF_TabBarController *tabBarVC = [[NJF_TabBarController alloc] init];
     self.window.rootViewController = tabBarVC;
+    [self lanuchAD];
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -54,5 +56,36 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+
+- (void)lanuchAD{
+    //设置你工程的启动页使用的是:LaunchImage 还是 LaunchScreen.storyboard(不设置默认:LaunchImage)
+    [XHLaunchAd setLaunchSourceType:SourceTypeLaunchImage];
+    //配置广告数据
+    XHLaunchImageAdConfiguration *imageAdconfiguration = [XHLaunchImageAdConfiguration new];
+    //广告停留时间
+    imageAdconfiguration.duration = 3;
+    //广告frame
+    imageAdconfiguration.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 0.8);
+    //广告图片URLString/或本地图片名(.jpg/.gif请带上后缀)
+    imageAdconfiguration.imageNameOrURLString = @"daoshi_man.png";
+    //设置GIF动图是否只循环播放一次(仅对动图设置有效)
+    imageAdconfiguration.GIFImageCycleOnce = NO;
+    //图片填充模式
+    imageAdconfiguration.contentMode = UIViewContentModeScaleAspectFill;
+    //广告点击打开页面参数(openModel可为NSString,模型,字典等任意类型)
+    imageAdconfiguration.openModel = @"http://wzcq.xy.com/";
+    //广告显示完成动画
+    imageAdconfiguration.showFinishAnimate =ShowFinishAnimateFlipFromLeft;
+    //广告显示完成动画时间
+    imageAdconfiguration.showFinishAnimateTime = 0.8;
+    //跳过按钮类型
+    imageAdconfiguration.skipButtonType = SkipTypeTimeText;
+    //后台返回时,是否显示广告
+    imageAdconfiguration.showEnterForeground = NO;
+    //设置要添加的子视图(可选)
+    //imageAdconfiguration.subViews = [self launchAdSubViews];
+    //显示开屏广告
+    [XHLaunchAd imageAdWithImageAdConfiguration:imageAdconfiguration delegate:self];
+}
 
 @end
